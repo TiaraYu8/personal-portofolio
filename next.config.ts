1e79a88245c1a55085857737346720b40bf9242f
TiaraYu8 <yuliandari.tiara@gmail.com>
2025-04-29 21:22:01 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// next.config.js
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  output: 'export', // penting: generate static export
+  basePath: isGithubPages ? '/personal-portofolio' : '',
+  assetPrefix: isGithubPages ? '/personal-portofolio/' : '',
 };
-
-export default nextConfig;
