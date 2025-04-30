1d8cc343a78a40a8a4a24f9e6e51b9f2908d96ab
TiaraYu8 <yuliandari.tiara@gmail.com>
2025-04-30 13:56:05 +0700

    adding nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   basePath: '/personal-portofolio',
   assetPrefix: '/personal-portofolio/',
   images: {
